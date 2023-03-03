c2799d91ea3a71565003edd14def925bbb92608f
-(Cooper)- <103378143+Cooper-VR@users.noreply.github.com>
2023-03-03 13:12:30 -0700

    cubeGameDone

---
+++ Assets/scripts/colliderSpawn.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colliderSpawn : MonoBehaviour
+{
+    #region publicVariables
+    public Transform playerPosition;
+    public GameObject collisionPrefab;
+
+    public float startTime;
+    #endregion
+
+    #region privateVariabes
+    private System.Random randomPositionCreator = new System.Random();
+    private Vector3 randomPosition;
+    private GameObject[] initalSpawns = new GameObject[20];
+    private float playerPositonZ;
+    public GameObject[] allObjects = new GameObject[500];
+    private float startPositonZ;
+    private int index;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPositonZ = playerPosition.position.z;
+        if (playerPositonZ - startPositonZ > 30f){
+            startPositonZ = playerPositonZ;
+            Destroy(allObjects[0]);
+            for (int i = 0; i < allObjects.Length - 1; i++){
+            // moving elements downwards, to fill the gap at [index]
+                allObjects[i] = allObjects[i + 1];
+            }
+            for (int i = 0; i < allObjects.Length - 1; i++){
+            // moving elements downwards, to fill the gap at [index]
+                if (allObjects[i] == null){
+                    index = i;
+                }
+            }
+            allObjects[index] = null;
+        }
+
+        if (Time.time - startTime > 0.15f){
+            startTime = Time.time;
+            randomPosition.x = randomPositionCreator.Next(-10, 10);
+            randomPosition.z = playerPosition.position.z + randomPositionCreator.Next(100, 200);
+            for (int i = 0; i < allObjects.Length; i++){
+                if (allObjects[i] == null){
+                    allObjects[i] = Object.Instantiate(collisionPrefab, randomPosition, new Quaternion(0, 0, 0, 0));
+                    break;
+                }
+            }
+            
+        }
+    }
+
+    public void initalSpawn(){
+        startPositonZ = playerPosition.position.z;
+
+        startTime = Time.time;
+        randomPosition.y = 0.5f;
+
+        for (int i  = 0; i < initalSpawns.Length; i++){
+            randomPosition.x = randomPositionCreator.Next(-8, 8);
+            randomPosition.z = playerPosition.position.z + randomPositionCreator.Next(20, 150);
+            initalSpawns[i] = Object.Instantiate(collisionPrefab, randomPosition, new Quaternion(0, 0, 0, 0));
+            allObjects[i] = initalSpawns[i];
+        }
+    }
+}
---
+++ Assets/scripts/playerMovment.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class playerMovment : MonoBehaviour
+{
+    #region publicVariables
+    public float speed = 200;
+    public Vector3 forceVector;
+    public Transform mainCamera;
+    public Vector3 CameraPosition;
+    public Transform ground;
+    public Text score;
+    public Transform playerPosition;
+
+    public GameObject uiPanel;
+    public GameObject startButton;
+    public colliderSpawn allObjects;
+    public GameObject[] obejcts  = new GameObject[500];
+    #endregion
+
+    #region  privateVariables
+    private Rigidbody rb;
+    private string baseText = "Score: ";
+    private Collider playerCollider;
+    private bool hit;
+    public Quaternion playerRotation;
+    #endregion
+
+    private void Start() {
+        obejcts = allObjects.allObjects;
+        allObjects.enabled = false;
+        playerRotation = transform.rotation;
+        uiPanel.SetActive(true);
+        startButton.SetActive(true);
+        rb = GetComponent<Rigidbody>();
+        hit = true;
+        playerCollider = GetComponent<Collider>();
+    }
+
+    void FixedUpdate()
+    {
+        positionChecker();
+        if (hit == false){
+            forceVector.z = speed;
+            CameraPosition = transform.position;
+            CameraPosition.z = CameraPosition.z - 10;
+            CameraPosition.y = 5;
+
+            ground.transform.position = new Vector3(0f, 0f, transform.position.z);
+
+            mainCamera.position = CameraPosition;
+
+            if (Input.GetKey(KeyCode.D)){
+                forceVector.x = 800;
+            } else if (Input.GetKey(KeyCode.A)){
+                forceVector.x = -800;
+            } else {
+                forceVector.x = 0;
+            }
+
+            if (rb.velocity.z < 50f){
+                rb.AddForce(forceVector.x * Time.deltaTime, 0, speed * Time.deltaTime);
+            } else {
+                rb.AddForce(forceVector.x * Time.deltaTime, 0, 0 * Time.deltaTime);
+            }
+            
+            score.text = baseText + Mathf.Round(transform.position.z).ToString();
+        }
+    }
+
+    void positionChecker(){
+        if (playerPosition.position.y < -50f){
+            hit = true;
+            allObjects.enabled = false;
+            Debug.Log(playerPosition.position.y);
+            Debug.Log("fell");
+            resetScene();
+        }
+    }
+
+    private void OnCollisionEnter(Collision playerCollider) {
+        if (Time.time > 1){
+            hit = true;
+            allObjects.enabled = false;
+            Debug.Log("hit");
+            resetScene();
+        }
+    }
+
+    public void startButtonClick(){
+        hit = false;
+        allObjects.enabled = true;
+        uiPanel.SetActive(false);
+        startButton.SetActive(false);
+        allObjects.initalSpawn();
+        transform.rotation = Quaternion.Euler( new Vector3(0, 0, 0));
+        
+    }
+
+    void resetScene(){
+        transform.position = new Vector3(0, 0.5f, 0);
+        uiPanel.SetActive(true);
+        startButton.SetActive(true);
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0, 0, 0);
+
+        for (int i = 0; i < obejcts.Length; i++){
+            Destroy(obejcts[i]);
+        }
+    }
+}
